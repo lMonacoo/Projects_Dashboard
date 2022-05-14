b5e44860cddc4bbadf762ea63dec5ff35a412aa2
lMonacoo <lucascansimonaco@yahoo.com.br>
2022-05-14 18:32:28 -0300

    ✨ [dashboard]: create speed dial tooltip open for fast access to features

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,21 +1,3 @@
-// import AllProductsStatusSelect from './all-products-status-select/all-products-status-select';
-// import FloaterMenuSort from './floater-menu-sort/floater-menu-sort';
-// import Header from './header/header';
-// import Message from './message/message';
-// import Pagination from './pagination/pagination';
-// import SearchBar from './search-bar/search-bar';
-// import UserGrid from './user-grid/user-grid';
-// import UserRoleSelect from './user-role-select/user-role-select';
-// import UserStatusSelect from './user-status-select/user-status-select';
+import { SpeedDialTooltipOpen } from './SpeedDialTooltipOpen';
 
-// export {
-//   FloaterMenuSort,
-//   Header,
-//   Message,
-//   Pagination,
-//   SearchBar,
-//   UserGrid,
-//   UserStatusSelect,
-//   UserRoleSelect,
-//   AllProductsStatusSelect
-// };
+export { SpeedDialTooltipOpen };
--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,3 +1,53 @@
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import Typography from '@mui/material/Typography';
+import { useTheme } from 'styled-components';
+
+import { SpeedDialTooltipOpen } from '~/components/molecules';
+import { useAppDispatch } from '~/hooks';
+import { logout } from '~/store';
+
 export const Dashboard = () => {
-  return <h1>DASHBOARD</h1>;
+  const { colors } = useTheme();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <Box sx={{ width: '100vw', height: '100vh' }}>
+      <h1>DASHBOARD</h1>
+      <Fab
+        onClick={handleLogout}
+        variant='extended'
+        size='medium'
+        color='primary'
+        aria-label='logout'
+      >
+        <Typography sx={{ fontWeight: 700, color: colors.white }} variant='body1'>
+          Logout
+        </Typography>
+        <LogoutIcon sx={{ ml: 1 }} style={{ fill: 'white', fontSize: 22 }} />
+      </Fab>
+
+      <SpeedDialTooltipOpen
+        actions={[
+          {
+            name: 'New User',
+            icon: <GroupAddIcon sx={{ fill: colors.greenTertiary }} />,
+            callback: () => console.log('new user')
+          },
+          {
+            name: 'New Project',
+            icon: <BookmarkAddIcon sx={{ fill: colors.greenTertiary }} />,
+            callback: () => console.log('new Project')
+          }
+        ]}
+      />
+    </Box>
+  );
 };
