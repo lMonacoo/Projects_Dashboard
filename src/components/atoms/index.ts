d0388168d0e08308770ba3c3c6c51efa14043ee7
lMonacoo <lucascansimonaco@yahoo.com.br>
2022-05-15 18:42:30 -0300

    🚀 [dist]: create githubpages

--- src/components/atoms/index.ts
+++
@@ -1,7 +0,0 @@
-// import Checkbox from './checkbox/checkbox';
-// import EmptyMessage from './empty-message/empty-message';
-
-// export { EmptyMessage, Checkbox };
-
-// export * from './buttons';
-// export * from './inputs';
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/projects-dashboard/',
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }]
   },
